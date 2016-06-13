3e4fb706e315577e3fce043336a29864c481ba64
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-13 12:18:43 +0200

    Add classes for missing service tasks

---
+++ ServiceTasks/src/main/java/org/miasi/jenkins/DeployApplication.java
@@ -0,0 +1,12 @@
+package org.miasi.jenkins;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class DeployApplication implements JavaDelegate {
+
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+    	// TODO
+    }
+
+}
